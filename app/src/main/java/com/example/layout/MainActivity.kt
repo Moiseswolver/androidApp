b4b7f3ec70d0071054bd8c73490c6de24fa530b4
pve_gsantana <pve_gsantana@uolinc.com>
2021-05-20 23:17:00 -0300

    Implementação do app

---
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickmebutton = findViewById<Button>(R.id.clickmebutton)
+        val stylesGroup = findViewById<androidx.constraintlayout.widget.Group>(R.id.stylesGroup)
+
+        clickmebutton.text ="click"
+
+        clickmebutton.setOnClickListener {
+            if (stylesGroup.visibility == View.INVISIBLE){
+                stylesGroup.visibility = View.INVISIBLE
+            } else {
+                stylesGroup.visibility = View.VISIBLE
+            }
+
+        }
+
+    }
+}
